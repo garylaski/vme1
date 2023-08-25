699a1b172ea0e7041106b8ca0c0fabc49ef72bac
gary <28818063+garylaski@users.noreply.github.com>
2023-08-24 22:38:15 -0400

    add simple visualizer

--- static/script.js
+++ static/script.js
@@ -27,8 +27,10 @@ ws.onmessage = function(evt) {
             sampleRate: sampleRate,
             flushingTime: 200
         });
+        init_visualizer(player);
     } else {
         player.feed(evt.data);
+        player.visualize();
     }
 }
 
@@ -42,3 +44,36 @@ ws.onerror = function(err) {
     console.log("Error: ", err);
 }
 
+function init_visualizer(player) {
+    player.analyser = player.audioCtx.createAnalyser();
+    player.gainNode.connect(player.analyser)
+    player.analyser.connect(player.audioCtx.destination)
+    const canvas = document.querySelector(".visualizer");
+    player.canvasCtx = canvas.getContext("2d"); 
+    player.analyser.fftSize = 256;
+    player.bufferLengthAlt = player.analyser.frequencyBinCount;
+    player.dataArrayAlt = new Uint8Array(player.bufferLengthAlt);
+    player.WIDTH = canvas.width;
+    player.HEIGHT = canvas.height;
+    player.barWidth = (player.WIDTH / player.bufferLengthAlt) * 2.5;
+    player.visualize = function() {
+        if (!this) return;
+        this.canvasCtx.clearRect(0, 0, this.WIDTH, this.HEIGHT);
+        this.drawVisual = requestAnimationFrame(this.visualize);
+        this.analyser.getByteFrequencyData(this.dataArrayAlt);
+        this.canvasCtx.fillStyle = "rgb(0, 0, 0)";
+        this.canvasCtx.fillRect(0, 0, this.WIDTH, this.HEIGHT);
+        this.x = 0;
+        for (let i = 0; i < this.bufferLengthAlt; i++) {
+            this.barHeight = this.dataArrayAlt[i];
+            this.canvasCtx.fillStyle = "rgb(" + (this.barHeight + 100) + ",50,50)";
+            this.canvasCtx.fillRect(
+                this.x,
+                this.HEIGHT - this.barHeight / 2,
+                this.barWidth,
+                this.barHeight / 2
+            );
+            this.x += this.barWidth + 1;
+        }
+    }
+}
