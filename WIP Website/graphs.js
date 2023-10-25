b0c7b911aaefb6381ff462c7a9a1c366e8727b63
myuan63 <myuan63@gatech.edu>
2023-10-25 09:51:30 -0400

    Added some vertical sliders and moved graph javascript into separate file

---
+++ WIP Website/graphs.js
@@ -0,0 +1,14 @@
+function showGraph(num)
+{
+    var graphs = document.getElementsByClassName("graph");
+    var graphSelectors = document.getElementsByClassName("graphSelector");
+    for (var i = 0; i < graphs.length; ++i)
+    {
+        graphs[i].style.display = "none";
+        graphSelectors[i].style.background = "white"
+    }
+    
+    graphs[num - 1].style.display = "block";
+    graphSelectors[num-1].style.background = "lightgrey"
+    return false;
+}
